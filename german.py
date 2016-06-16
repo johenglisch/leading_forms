7905c21a345ec3d0106a7dac6adafa8b6478075d
Johannes Englisch <cyberjoe0815@hotmail.com>
2016-06-16 08:06:03 +0200

    removed main funcs

--- german.py
+++ german.py
@@ -3,23 +3,18 @@
 from leading_forms import *
 
 
-def main():
-    leading_forms = [
-        LeadingForm(1, 'r', {'masc': True,  'fem': False, 'gov': False, 'obl': False}),
-        LeadingForm(2, 'n', {'masc': True,  'fem': False, 'gov': True,  'obl': False}),
-        LeadingForm(3, 'm', {'masc': True,  'fem': False, 'gov': True,  'obl': True}),
-        LeadingForm(4, 's', {'masc': True,  'fem': False, 'gov': False, 'obl': True}),
-        LeadingForm(5, 's', {'masc': True,  'fem': True,  'gov': True,  'obl': False}),
-        LeadingForm(6, 'e', {'masc': False, 'fem': True,  'gov': False, 'obl': False}),
-        LeadingForm(7, 'n', {'masc': False, 'fem': False, 'gov': True,  'obl': True}),
-        LeadingForm(8, 'r', {'masc': False, 'fem': True,  'gov': False, 'obl': True}),
-        LeadingForm(9, 'r', {'masc': False, 'fem': False, 'gov': False, 'obl': True})]
+leading_forms = [
+    LeadingForm(1, 'r', {'masc': True,  'fem': False, 'gov': False, 'obl': False}),
+    LeadingForm(2, 'n', {'masc': True,  'fem': False, 'gov': True,  'obl': False}),
+    LeadingForm(3, 'm', {'masc': True,  'fem': False, 'gov': True,  'obl': True}),
+    LeadingForm(4, 's', {'masc': True,  'fem': False, 'gov': False, 'obl': True}),
+    LeadingForm(5, 's', {'masc': True,  'fem': True,  'gov': True,  'obl': False}),
+    LeadingForm(6, 'e', {'masc': False, 'fem': True,  'gov': False, 'obl': False}),
+    LeadingForm(7, 'n', {'masc': False, 'fem': False, 'gov': True,  'obl': True}),
+    LeadingForm(8, 'r', {'masc': False, 'fem': True,  'gov': False, 'obl': True}),
+    LeadingForm(9, 'r', {'masc': False, 'fem': False, 'gov': False, 'obl': True})]
 
-    constraints = [match, ident('masc'), ident('obl'), ident('fem'), ident('gov')]
+constraints = [match, ident('masc'), ident('obl'), ident('fem'), ident('gov')]
 
-    paradigm = Paradigm(['obl', 'gov'], ['masc', 'fem'], leading_forms, constraints)
-    print(paradigm)
-
-
-if __name__ == '__main__':
-    main()
+paradigm = Paradigm(['obl', 'gov'], ['masc', 'fem'], leading_forms, constraints)
+print(paradigm)
--- russian.py
+++ russian.py
@@ -20,51 +20,38 @@ def match_gov_cl2_sg(paradigm_cell, candidate):
     return 0
 
 
-def main():
-    leading_forms = [
-        LeadingForm(1, 'a',      {'a': False, 'b': True,  'gov': False, 'subj': True,  'obl': False}),
-        LeadingForm(2, 'u',      {'a': False, 'b': True,  'gov': True,  'subj': False, 'obl': False}),
-        LeadingForm(3, 'y',      {'a': False, 'b': True,  'gov': True,  'subj': True,  'obl': False}),
-        LeadingForm(4, '\u2205', {'a': True,  'b': False, 'gov': False, 'subj': True,  'obl': False}),
-        LeadingForm(5, 'a',      {'a': True,  'b': False, 'gov': True,  'subj': True,  'obl': False})]
+leading_forms = [
+    LeadingForm(1, 'a',      {'a': False, 'b': True,  'gov': False, 'subj': True,  'obl': False}),
+    LeadingForm(2, 'u',      {'a': False, 'b': True,  'gov': True,  'subj': False, 'obl': False}),
+    LeadingForm(3, 'y',      {'a': False, 'b': True,  'gov': True,  'subj': True,  'obl': False}),
+    LeadingForm(4, '\u2205', {'a': True,  'b': False, 'gov': False, 'subj': True,  'obl': False}),
+    LeadingForm(5, 'a',      {'a': True,  'b': False, 'gov': True,  'subj': True,  'obl': False})]
 
-    constraints = [
-        match_gov_cl2_sg,
-        no_gov_inanim,
-        match,
-        ident('a'), ident('b'),
-        ident('gov'), ident('obl'), ident('subj')]
+constraints = [
+    match_gov_cl2_sg,
+    no_gov_inanim,
+    match,
+    ident('a'), ident('b'),
+    ident('gov'), ident('obl'), ident('subj')]
 
-    paradigm = Paradigm(
-        ['obl', 'gov', 'subj'],
-        ['anim', 'a', 'b'],
-        leading_forms,
-        constraints)
+paradigm = Paradigm(
+    ['obl', 'gov', 'subj'],
+    ['anim', 'a', 'b'],
+    leading_forms,
+    constraints)
 
-    paradigm.rows = [
-        {'obl': False, 'gov': False, 'subj': True},
-        {'obl': False, 'gov': True,  'subj': False},
-        {'obl': False, 'gov': True,  'subj': True}]
+paradigm.rows = [
+    {'obl': False, 'gov': False, 'subj': True},
+    {'obl': False, 'gov': True,  'subj': False},
+    {'obl': False, 'gov': True,  'subj': True}]
 
-    paradigm.columns = [
-        {'anim': False, 'a': True,  'b': False},
-        {'anim': False, 'a': False, 'b': True},
-        {'anim': True,  'a': True,  'b': False},
-        {'anim': True,  'a': False, 'b': True}]
+paradigm.columns = [
+    {'anim': False, 'a': True,  'b': False},
+    {'anim': False, 'a': False, 'b': True},
+    {'anim': True,  'a': True,  'b': False},
+    {'anim': True,  'a': False, 'b': True}]
 
-    print(paradigm)
+print(paradigm)
 
-    print(tableau(
-        constraints, paradigm.candidates,
-        {'obl': False, 'gov': True, 'subj': False, 'a': True, 'b': False, 'anim': False},
-        10))
-
-    print(tableau(
-        constraints, paradigm.candidates,
-        {'obl': False, 'gov': True, 'subj': False, 'a': False, 'b': True, 'anim': False},
-        10))
-
-
-
-if __name__ == '__main__':
-    main()
+print(paradigm.derivation('-obl +gov -subj +a -b -anim', 10))
+print(paradigm.derivation('-obl +gov -subj +a -b +anim', 10))
